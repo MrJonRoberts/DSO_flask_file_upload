f02ee148f8c0337326314793e3b5ae00374afd39
MrJonRoberts <jroberts@tas.qld.edu.au>
2022-03-04 12:52:21 +1000

    file upload

--- app.py
+++ app.py
@@ -1,11 +1,30 @@
-from flask import Flask, render_template
-
+from flask import Flask, abort, render_template, request, redirect, url_for
+from werkzeug.utils import secure_filename
+import os
 app = Flask(__name__)
+# set up the folders
+UPLOAD_FOLDER = 'UPLOADS'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 
-@app.route("/")
+
+
+@app.route('/')
 def home():
     return render_template("home.html")
 
+
+@app.route('/upload',methods = ['GET','POST'])
+
+def upload_file():
+    if request.method == 'POST':
+        file = request.files['file']
+        if file:
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            msg="File saved"
+            return render_template("home.html", msg=msg)
+
+    return render_template('form.html')
 if __name__ == '__main__':
     app.run()
