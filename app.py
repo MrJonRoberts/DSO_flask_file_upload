60808fc7569e36e347b7014babd7f62e8fd82f5f
MrJonRoberts <jroberts@tas.qld.edu.au>
2022-03-04 12:54:15 +1000

    file upload

--- app.py
+++ app.py
@@ -11,20 +11,17 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 @app.route('/')
 def home():
-    return render_template("home.html")
-
-
-@app.route('/upload',methods = ['GET','POST'])
-
-def upload_file():
     if request.method == 'POST':
         file = request.files['file']
         if file:
             filename = secure_filename(file.filename)
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            msg="File saved"
+            msg = "File saved"
             return render_template("home.html", msg=msg)
 
-    return render_template('form.html')
+    return render_template('home.html')
+
+
+
 if __name__ == '__main__':
     app.run()
